9fb6575cdc0b9855605d43e725970fc86752c51e
aman-uta-kgp <165545216+aman-uta-kgp@users.noreply.github.com>
2024-06-29 07:04:55 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "//aman-uta-kgp.github.io/portfolio/precache-manifest.980fcf1ba1f7f8ea919f4f526de30c31.js"
+  "/portfolio/precache-manifest.3dc7ebadecd549ba85f7df6f2eff4567.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("//aman-uta-kgp.github.io/portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
