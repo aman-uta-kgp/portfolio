55c44d087de029886f354dbbf772b09ab880a60a
Aman Sharma <165545216+aman-uta-kgp@users.noreply.github.com>
2024-08-19 19:49:28 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.7de0a7c733ec8b409658a39282acfed7.js"
+  "/portfolio/precache-manifest.7cafece2aaa4d99a66c71af2e815e26c.js"
 );
 
 workbox.clientsClaim();
